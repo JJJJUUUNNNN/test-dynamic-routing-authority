bbb971c9a973509a78bc77cb6fc3f57196570b74
JJJJUUUNNNN <1902958347@qq.com>
2023-07-13 12:20:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Layout from './Layout/index.vue'
+
+</script>
+
+<template>
+  <Layout>
+    <router-view></router-view>
+  </Layout>
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import {useRouter,useRoute} from 'vue-router'
+
+createApp(App).mount('#app')
