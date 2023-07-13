29912485f3d1571c9acefb6f4fd3695074e56af5
JJJJUUUNNNN <1902958347@qq.com>
2023-07-13 12:24:09 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Layout from './Layout/index.vue'
+import tableVue from './view/table.vue';
 
 </script>
 
 <template>
   <Layout>
+    <!-- <tableVue></tableVue> -->
     <router-view></router-view>
   </Layout>
 </template>
