82c2c43c50e7f96a38d8a7d808ca33d2c5588237
JJJJUUUNNNN <1902958347@qq.com>
2023-07-23 23:59:12 +0800

    1

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './style.css';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+import './router/permission';
 
 const pinia = createPinia();
 
